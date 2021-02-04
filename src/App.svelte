6ef23d2c5498a782641752e1386f1e383ea58829
Maxime <koitsalu@gmail.com>
2021-02-04 10:34:43 +0100

    feat: add website

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<main>
+	<h1>KOKODO</h1>
+</main>
+
+<style>
+	main {
+		background-color: #111;
+		font-family: "Major Mono Display";
+		text-align: center;
+		color: #FFF;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		letter-spacing: 1px;
+	}
+
+	h1 {
+		background-image: url("/images/effect.gif");
+		background-size: cover;
+		color: transparent;
+		-moz-background-clip: text;
+		-webkit-background-clip: text;
+		text-transform: uppercase;
+		font-size: 120px;
+		margin: 10px 0;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
